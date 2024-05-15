0046399cc0c3870961efb16817f8e078fecf3e58
marcusrosadev <marcusrosadev@gmail.com>
2024-05-15 14:53:32 -0300

    feat: edit/delete/bid buttons behvaior depends on been logged or not

--- src/js/controllers/actions/logged.js
+++ src/js/controllers/actions/logged.js
@@ -13,6 +13,13 @@ export function updateButtonVisibility() {
     document.querySelector('.register-modal-btn').style.display = 'none';
     document.querySelector('.logout-btn').style.display = 'inline-block';
     document.querySelector('.profile-btn').style.display = 'inline-block';
+
+    //single-item button logic
+    document.querySelector('#bidButton').removeAttribute('data-bs-target');
+    document.querySelector('#bidButton').removeAttribute('data-bs-toggle');
+    document.querySelector('#bidButton').classList.add('bidButton');
+    document.querySelector('#edit-btn').classList.remove('d-none');
+    document.querySelector('#delete-btn').classList.remove('d-none');
   } else {
     document.querySelector('.profile-btn').style.display = 'none';
     document.querySelector('.logout-btn').style.display = 'none';
